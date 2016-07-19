c142efac31ff5236de567b27c113ea355d1aecb2
DeusExMachina <deusxmachina96@gmail.com>
2016-07-19 17:08:44 +0200

    Add login and register pages

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,13 @@ router.get('/', function(req, res, next) {
 });
 
 
+/* User sign-up page */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 module.exports = router;

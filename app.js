47f8c96c72a535f9887adda097093465d689d634
DeusExMachina <deusxmachina96@gmail.com>
2016-07-27 22:44:03 +0200

    Fix dotenv getting called in production

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var dotenv = require('dotenv').config({path: './config.env'});
-var expressSession = require('express-session');
 
+var expressSession = require('express-session');
+var app = express();
+if(app.get('env') === 'development')
+  var dotenv = require('dotenv').config({path: './config.env'});
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

dc0f3c594ed84ede2197223b6a514fc1cded7afb
DeusExMachina <deusxmachina96@gmail.com>
2016-07-27 23:11:07 +0200

    Fix a bug that would occur if a book from API call had no author. Add notification if no books were found, and credits to openlibrary.org

--- public/javascripts/book_lookup.js
+++ public/javascripts/book_lookup.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#book-find").click(function(){
        var title = $("#title").val();
-        var query = "http://openlibrary.org/search.json?title=" + encodeURIComponent(title);
+        var query = "https://openlibrary.org/search.json?title=" + encodeURIComponent(title);
         console.log(query);
         $.getJSON(query, function(data) {
             console.log(data);
@@ -9,17 +9,23 @@ $(document).ready(function(){
             results = results.filter(function(element) {
                return element.cover_i != null;
             });
-            for(var i = 0; i < results.length; i += 4) {
-                $(".overlay").append("<div class='row'>");
-                for(var k = 0; i + k < results.length && k < 4; k++) {
-                    $(".overlay").append("<div class='small-12 medium-3 columns'>" +
-                        "<img id='" + (i + k) +
-                        "' class='cover-image' src='http://covers.openlibrary.org/b/id/" + results[i + k].cover_i + "-M.jpg'>" +
-                        "<p class='cover-info'><span class='cover-title'>" + results[i + k].title +
-                        "</span> by <span class='cover-author'>" + results[i + k].author_name[0] + "</span></p>" +
-                        "</li></div>");
-                }
+            if(results.length == 0)
+                $(".overlay-header").text("No books with that title found.");
+            else {
+                $(".overlay-header").text("Pick a book that fits your search.");
+                for (var i = 0; i < results.length; i += 4) {
+                    $(".overlay").append("<div class='row'>");
+                    for (var k = 0; i + k < results.length && k < 4; k++) {
+                        var author_name = results[i + k].author_name ? results[i + k].author_name[0] : "Unknown";
+                        $(".overlay").append("<div class='small-12 medium-3 columns'>" +
+                            "<img id='" + (i + k) +
+                            "' class='cover-image' src='http://covers.openlibrary.org/b/id/" + results[i + k].cover_i + "-M.jpg'>" +
+                            "<p class='cover-info'><span class='cover-title'>" + results[i + k].title +
+                            "</span> by <span class='cover-author'>" + author_name + "</span></p>" +
+                            "</li></div>");
+                    }
 
+                }
             }
             $(".overlay").show();
             $('.cover-image').click(function() {
